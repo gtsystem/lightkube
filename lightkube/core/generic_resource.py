56a2ef55ccb38da8f2e55e6e56e6aee0ce41e767
Giuseppe Tribulato <giuseppe.tribulato@here.com>
2020-08-28 22:56:30 +0200

    Add support for generic unstructured resources

---
+++ lightkube/core/generic_resource.py
@@ -0,0 +1,114 @@
+from typing import Type, Any, Optional
+
+from lightkube.base import resource as res
+from lightkube.models import meta_v1, autoscaling_v1
+
+__all__ = ['create_global_resource', 'create_namespaced_resource']
+
+
+class Generic(dict):
+    @property
+    def apiVersion(self) -> str:
+        return self.get('apiVersion')
+
+    @property
+    def kind(self) -> str:
+        return self.get('kind')
+
+    @property
+    def metadata(self) -> Optional[meta_v1.ObjectMeta]:
+        meta = self.get('metadata')
+        if meta is None:
+            return None
+        return meta_v1.ObjectMeta.from_dict(meta)
+
+    def __getattr__(self, item):
+        if item.startswith("_"):
+            raise AttributeError(f"{item} not found")
+        return self[item]
+
+    @classmethod
+    def from_dict(cls, d: dict, lazy=True):
+        return cls(d)
+
+    def to_dict(self, dict_factory=dict):
+        return dict_factory(self)
+
+
+def create_api_info(group, version, kind, plural, verbs=None) -> res.ApiInfo:
+    if verbs is None:
+        verbs = ['delete', 'deletecollection', 'get', 'global_list', 'global_watch', 'list', 'patch',
+               'post', 'put', 'watch']
+    return res.ApiInfo(
+        resource=res.ResourceDef(group, version, kind),
+        plural=plural,
+        verbs=verbs
+    )
+
+
+class GenericGlobalScale(res.GlobalSubResource, autoscaling_v1.Scale):
+    pass
+
+
+class GenericGlobalStatus(res.GlobalSubResource, Generic):
+    pass
+
+
+class GenericNamespacedScale(res.NamespacedResourceG, autoscaling_v1.Scale):
+    pass
+
+
+class GenericNamespacedStatus(res.NamespacedResourceG, Generic):
+    pass
+
+
+def _create_subresource(main_class, parent_info: res.ApiInfo, action):
+    class TmpName(main_class):
+        api_info = res.ApiInfo(
+            resource=parent_info.resource if action == 'status' else res.ResourceDef('autoscaling', 'v1', 'Scale'),
+            parent=parent_info.resource,
+            plural=parent_info.plural,
+            verbs=['get', 'patch', 'put'],
+            action=action,
+        )
+
+    TmpName.__name__ = TmpName.__qualname__ = f"{parent_info.resource.kind}{action.capitalize()}"
+    return TmpName
+
+
+class GenericGlobalResource(res.GlobalResource, Generic):
+    Scale: Type[GenericGlobalScale]
+    Status: Type[GenericGlobalStatus]
+
+
+class GenericNamespacedResource(res.NamespacedResourceG, Generic):
+    Scale: Type[GenericNamespacedScale]
+    Status: Type[GenericNamespacedStatus]
+
+
+def _create_resource(namespaced, group, version, kind, plural, verbs=None) -> Any:
+    if namespaced:
+        main, status, scale = GenericNamespacedResource, GenericNamespacedStatus, GenericNamespacedScale
+    else:
+        main, status, scale = GenericGlobalResource, GenericGlobalStatus, GenericGlobalScale
+
+    class TmpName(main):
+        api_info = create_api_info(group, version, kind, plural, verbs=verbs)
+
+        Scale = _create_subresource(scale, api_info, action='scale')
+        Status = _create_subresource(status, api_info, action='status')
+
+    TmpName.__name__ = TmpName.__qualname__ = kind
+    return TmpName
+
+
+def create_global_resource(group, version, kind, plural, verbs=None) \
+        -> Type[GenericGlobalResource]:
+    return _create_resource(
+        GenericGlobalResource, group, version, kind, plural, verbs=verbs)
+
+
+def create_namespaced_resource(group, version, kind, plural, verbs=None) \
+        -> Type[GenericNamespacedResource]:
+    return _create_resource(
+        GenericNamespacedResource, group, version, kind, plural, verbs=verbs)
