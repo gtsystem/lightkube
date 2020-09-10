a0b5e371dca22c71a653a2198fe79a69cf368ace
Giuseppe Tribulato <gtsystem@users.noreply.github.com>
2020-09-10 21:01:00 +0200

    setup: Added extended documentation

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
+from pathlib import Path
 
 setup(
     name='lightkube',
-    version="0.2.0",
+    version="0.2.1",
     description='Lightweight kubernetes client library',
-    long_description='Lightweight kubernetes client library',
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
     author='Giuseppe Tribulato',
     author_email='gtsystem@gmail.com',
     license='Apache Software License',
