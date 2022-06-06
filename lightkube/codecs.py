4446c4093698637d646b57e37a021abe9da1ef6d
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-06 16:41:25 -0400

    Revert "feat: add implicit generic model creation to from_dict and load_all_yaml"
    This reverts commit 2e7e562a5de3bd55ccecdac82aa12c568d49402e.

--- lightkube/codecs.py
+++ lightkube/codecs.py
@@ -3,11 +3,8 @@ from typing import Union, TextIO, List
 
 import yaml
 
-from . import Client
-from .generic_resource import get_generic_resource, GenericGlobalResource, GenericNamespacedResource, \
-    create_namespaced_resource, create_global_resource
+from .generic_resource import get_generic_resource, GenericGlobalResource, GenericNamespacedResource
 from .core.exceptions import LoadResourceError
-from .resources.apiextensions_v1 import CustomResourceDefinition
 
 try:
     import jinja2
@@ -19,79 +16,31 @@ REQUIRED_ATTR = ('apiVersion', 'kind')
 AnyResource = Union[GenericGlobalResource, GenericNamespacedResource]
 
 
-def _load_model(version, kind, client=None):
+def _load_model(version, kind):
     if "/" in version:
         group, version_n = version.split("/")
+        # Check if a generic resource was defined
+        model = get_generic_resource(version, kind)
+        if model is not None:
+            return model
+
+        # Generic resource not defined, but it could be a k8s resource
         if group.endswith(".k8s.io"):
-            # k8s resource
             group = group[:-7]
-            group = group.replace(".", "_")
-            version = "_".join([group, version_n])
-        else:
-            # Generic resource.  Check if it has been defined, and define it if not.
-            # Check if a generic resource was defined
-            model = get_generic_resource(version, kind)
-            if model is not None:
-                return model
-            if client is not None:
-                try:
-                    model = _create_model_from_client(kind, version_n, client)
-                except KeyError as e:
-                    raise LoadResourceError(
-                        f"Unable to implicitly load resource {kind}."
-                        f"  Got error: '{e}'"
-                    )
-                return model
+        group = group.replace(".", "_")
+        version = "_".join([group, version_n])
     else:
         version = f'core_{version}'
 
     try:
         module = importlib.import_module(f'lightkube.resources.{version.lower()}')
     except ImportError as e:
+        # It was not a k8s resource and a generic resource was not previously defined
         raise LoadResourceError(f"{e}. If using a CRD, ensure you define a generic resource.")
     return getattr(module, kind)
 
 
-def _create_model_from_client(kind, version_n, client):
-    """Creates a generic resource model from a k8s client.
-
-    TODO: Currently assumes a namespaced resource.  Need to check spec for scope==namespaced, etc
-    """
-    crd = _get_crd_of_kind(kind, client)
-    if crd.spec.scope == "Namespaced":
-        creator = create_namespaced_resource
-    elif crd.spec.scope == "Cluster":
-        creator = create_global_resource
-    else:
-        raise ValueError(
-            f"Unexpected scope for resource of kind {kind}.  Expected 'Namespaced' or 'Cluster',"
-            f" got {crd.spec.scope}"
-        )
-
-    model = creator(**_crd_to_dict(crd, version_n))
-    return model
-
-
-def _crd_to_dict(crd, version_n):
-    return {
-        "group": crd.spec.group,
-        "version": version_n,
-        "kind": crd.spec.names.kind,
-        "plural": crd.spec.names.plural,
-    }
-
-
-def _get_crd_of_kind(kind: str, client: Client):
-    crds = client.list(CustomResourceDefinition)
-    crd = next((c for c in crds if c.spec.names.kind == kind), None)
-    if crd:
-        return crd
-    else:
-        raise KeyError(f"Could not find CRD for kind {kind} in Kubernetes cluster")
-
-
-
-def from_dict(d: dict, client=None) -> AnyResource:
+def from_dict(d: dict) -> AnyResource:
     """Converts a kubernetes resource defined as python dict to the corresponding resource object.
     If the dict represent a standard resource, the function will automatically load the appropriate
     resource type. Generic resources are also supported and used assuming they were defined prior to
@@ -101,18 +50,16 @@ def from_dict(d: dict, client=None) -> AnyResource:
 
     * **d** - A dictionary representing a Kubernetes resource. Keys `apiVersion` and `kind` are
       always required.
-    * **client** - (Optional) A lightkube.Client used to infer the Resource definition for any 
-                   unknown CR
     """
     for attr in REQUIRED_ATTR:
         if attr not in d:
             raise LoadResourceError(f"Invalid resource definition, key '{attr}' missing.")
 
-    model = _load_model(d['apiVersion'], d['kind'], client=client)
+    model = _load_model(d['apiVersion'], d['kind'])
     return model.from_dict(d)
 
 
-def load_all_yaml(stream: Union[str, TextIO], context: dict = None, template_env = None, client=None) -> List[AnyResource]:
+def load_all_yaml(stream: Union[str, TextIO], context: dict = None, template_env = None) -> List[AnyResource]:
     """Load kubernetes resource objects defined as YAML. See `from_dict` regarding how resource types are detected.
     Returns a list of resource objects or raise a `LoadResourceError`.
 
@@ -124,8 +71,6 @@ def load_all_yaml(stream: Union[str, TextIO], context: dict = None, template_env
         will be used during templating.
     * **template_env** - `jinja2` template environment to be used for templating. When absent a standard
         environment is used.
-    * **client** - (Optional) A lightkube.Client used to infer the Resource definition for any 
-                   unknown CR
 
     **NOTE**: When using the template functionality (setting the context parameter), the dependency
         module `jinja2` need to be installed.
@@ -134,7 +79,7 @@ def load_all_yaml(stream: Union[str, TextIO], context: dict = None, template_env
         stream = _template(stream, context=context, template_env=template_env)
     res = []
     for obj in yaml.safe_load_all(stream):
-         res.append(from_dict(obj, client=client))
+         res.append(from_dict(obj))
     return res
 
 
