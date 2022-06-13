d106b5f893dc81d37472c488eadd0575d01042c1
Andrew Scribner <ca.scribner+1@gmail.com>
2022-06-13 15:11:20 -0400

    refactor based on review feedback
    Moves sort_objects to a new sort_objects.py.  Misc other minor refactors.

--- lightkube/__init__.py
+++ lightkube/__init__.py
@@ -1,4 +1,5 @@
 from .core.client import Client, AsyncClient
 from .core.generic_client import ALL_NS
 from .core.exceptions import ApiError, ConfigError, LoadResourceError
+from .core.sort_objects import sort_objects
 from .config.kubeconfig import KubeConfig, SingleConfig
