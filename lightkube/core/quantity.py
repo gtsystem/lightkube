f912a05fdecf1edbfda6c26263affd32e6eabb24
sed-i <82407168+sed-i@users.noreply.github.com>
2022-07-04 12:32:53 -0400

    add function to parse quantity

---
+++ lightkube/core/quantity.py
@@ -0,0 +1,49 @@
+import decimal
+import re
+
+multipliers = {
+    # Bytes
+    "m": (10, -3),  # 1000^(-1) (=0.001)
+    "": (10, 0),  # 1000^0 (=1)
+    "k": (10, 3),  # 1000^1
+    "M": (10, 6),  # 1000^2
+    "G": (10, 9),  # 1000^3
+    "T": (10, 12),  # 1000^4
+    "P": (10, 15),  # 1000^5
+    "E": (10, 18),  # 1000^6
+    "Z": (10, 21),  # 1000^7
+    "Y": (10, 24),  # 1000^8
+
+    # Bibytes
+    "Ki": (1024, 1),  # 2^10
+    "Mi": (1024, 2),  # 2^20
+    "Gi": (1024, 3),  # 2^30
+    "Ti": (1024, 4),  # 2^40
+    "Pi": (1024, 5),  # 2^50
+    "Ei": (1024, 6),  # 2^60
+    "Zi": (1024, 7),  # 2^70
+    "Yi": (1024, 8),  # 2^80
+}
+
+
+def parse_quantity(quantity: str) -> decimal.Decimal:
+    pat = re.compile(r"([+-]?\d+(?:[.]\d*)?(?:e[+-]?\d+)?|[.]\d+(?:e[+-]?\d+)?)(.*)")
+    match = pat.match(quantity)
+
+    if not match:
+        raise ValueError("Invalid quantity string: {}".format(quantity))
+
+    try:
+        value = decimal.Decimal(match.group(1))
+    except ArithmeticError as e:
+        raise ValueError("Invalid numerical value") from e
+
+    unit = match.group(2)
+
+    try:
+        base, exp = multipliers[unit]
+    except KeyError:
+        raise ValueError("Invalid unit suffix: {}".format(unit))
+
+    bibytes = value * base ** exp
+    return bibytes.quantize(decimal.Decimal("0.001"), rounding=decimal.ROUND_UP)
---
+++ tests/test_quantity.py
@@ -0,0 +1,36 @@
+import decimal
+import pytest
+
+from lightkube.core.quantity import parse_quantity
+
+
+def test_unitless():
+    """Unitless values must be interpreted as decimal notation."""
+    assert parse_quantity("1.5") == decimal.Decimal("1.5")
+    assert parse_quantity("0.30000000000000004") == decimal.Decimal("0.301")
+    assert parse_quantity("0.09999999999999998") == decimal.Decimal("0.1")
+    assert parse_quantity("3.141592653") == decimal.Decimal("3.142")
+
+
+def test_binary_notation():
+    assert parse_quantity("1.5Gi") == decimal.Decimal("1610612736")
+    assert parse_quantity("0.9Gi") == decimal.Decimal("966367641.6")
+
+
+def test_decimal_notation():
+    assert parse_quantity("1.5G") == decimal.Decimal("1500000000")
+    assert parse_quantity("0.9G") == decimal.Decimal("900000000")
+
+
+def test_invalid_value():
+    with pytest.raises(ValueError):
+        parse_quantity("1.2.3")
+    with pytest.raises(ValueError):
+        parse_quantity("1e2.3")
+
+
+def test_invalid_unit():
+    with pytest.raises(ValueError):
+        parse_quantity("1kb")
+    with pytest.raises(ValueError):
+        parse_quantity("1GGi")
