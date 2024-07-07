7724c29f41ac2ab3e580c3068a8ca4f00b64146c
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2024-07-07 13:32:10 -0500

    Set __all__

--- lightkube/__init__.py
+++ lightkube/__init__.py
@@ -4,3 +4,15 @@ from .core.generic_client import ALL_NS
 from .core.exceptions import ApiError, ConfigError, LoadResourceError
 from .core.sort_objects import sort_objects
 from .config.kubeconfig import KubeConfig, SingleConfig
+
+__all__ = [
+    "Client",
+    "AsyncClient",
+    "ALL_NS",
+    "ApiError",
+    "ConfigError",
+    "LoadResourceError",
+    "sort_objects",
+    "KubeConfig",
+    "SingleConfig",
+]
