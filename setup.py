85b75371a77b2a6d4b608a4014803b127d5d929e
Giuseppe Tribulato <gtsystem@gmail.com>
2024-12-05 14:28:32 +0100

    Fix #78: Setup now requires httpx != 0.28.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='lightkube',
-    version="0.15.5",
+    version="0.15.6",
     description='Lightweight kubernetes client library',
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setup(
     package_data={'lightkube': ['py.typed']},
     install_requires=[
         'lightkube-models >= 1.15.12.0',
-        'httpx >= 0.24.0',
+        'httpx >= 0.24.0, != 0.28.0, < 1.0.0',
         'PyYAML'
     ],
     extras_require={
