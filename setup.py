62bffa881bc1382ec28adbe495f3d5d086dc4430
Giuseppe Tribulato <gtsystem@gmail.com>
2024-12-16 14:05:43 +0100

    Version increase

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='lightkube',
-    version="0.15.7",
+    version="0.15.8",
     description='Lightweight kubernetes client library',
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
