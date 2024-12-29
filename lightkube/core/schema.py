e42edaa80dfe50fb1b00836163b65ef985a15ddf
Giuseppe Tribulato <gtsystem@gmail.com>
2024-12-29 18:25:55 +0100

    Fix autocomplete regression in vscode introduced in #74

--- lightkube/core/schema.py
+++ lightkube/core/schema.py
@@ -3,5 +3,6 @@ This module exposes dependencies used by lightkube-models
 
 These dependencies are here because we may decide to replace dataclasses with something else in the future
 """
-
-
+__all__ = ["dataclass", "field", "DictMixin"]
+from dataclasses import dataclass, field
+from .dataclasses_dict import DataclassDictMixIn as DictMixin

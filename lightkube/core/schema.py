gtsystem/lightkube

commit 75c71426d23963be94412ca6f26f77a7a61ab363

bump version

setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='lightkube',
-    version="0.15.3",
+    version="0.15.4",
     description='Lightweight kubernetes client library',
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",

commit e42edaa80dfe50fb1b00836163b65ef985a15ddf

Fix autocomplete regression in vscode introduced in #74

lightkube/core/schema.py
@@ -3,5 +3,6 @@ This module exposes dependencies used by lightkube-models
 
 These dependencies are here because we may decide to replace dataclasses with something else in the future
 """
-
-
+__all__ = ["dataclass", "field", "DictMixin"]
+from dataclasses import dataclass, field
+from .dataclasses_dict import DataclassDictMixIn as DictMixin

setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='lightkube',
-    version="0.16.1",
+    version="0.16.2",
     description='Lightweight kubernetes client library',
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",