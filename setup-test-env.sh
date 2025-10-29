585f6aac0a47fed6ff49f3ec822fe680e79dba0b
Jonas Dedden <university@jonas-dedden.de>
2025-10-29 22:49:31 +0100

    Move to `uv` and a modern `pyproject.toml`

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# This script wouldn't be required if models and resources move into a proper 'lightkube_models' package and are not
+# symlinked into the source tree
+
 if [ -d "../lightkube-models" ]; then
   SOURCE_DIR=$(python -c "import os.path; print(os.path.realpath('../lightkube-models'))")
 else
   SOURCE_DIR=$(python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-  pip install lightkube-models
+  uv pip install lightkube-models
 fi
 rm -f lightkube/models lightkube/resources
 ln -s  $SOURCE_DIR/lightkube/models lightkube
