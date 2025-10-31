daabdd24a558772d7262604ca9407bd2fea6dc29
Jonas Dedden <university@jonas-dedden.de>
2025-10-31 15:02:52 +0100

    Fix CI

--- setup-test-env.sh
+++ setup-test-env.sh
@@ -7,11 +7,16 @@ set -euo pipefail
 
 if [ -d "../lightkube-models" ]; then
   echo "Found 'lightkube-models' locally."
-  SOURCE_DIR=$(uv run python -c "import os.path; print(os.path.realpath('../lightkube-models'))")
+  SOURCE_DIR=$(realpath "../lightkube-models")
 else
-  echo "Did not find 'lightkube-models' locally, will install them from upstream..."
-  SOURCE_DIR=$(uv run python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
-  uv pip install lightkube-models
+  echo "Did not find 'lightkube-models' locally. Will use the venv's"
+  SOURCE_DIR=$(uv run --no-project python -c 'import site; print(site.getsitepackages()[0])')
+  if ! uv pip show lightkube-models then
+    echo "Package 'lightkube-models' isn't installed, will install upstream version."
+    uv pip install lightkube-models
+  else
+    echo "Package 'lightkube-models' is installed."
+  fi
 fi
 echo "Source dir of 'lightkube-models' is ${SOURCE_DIR}, will link that into the source tree."
 rm -f lightkube/models lightkube/resources
